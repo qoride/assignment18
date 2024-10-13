688c09ce9612cda88b12f1e020a748a494a69b59
qoride <qoride@gmail.com>
2024-10-13 02:42:29 +0000

    setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <limits>
 #include <vector>
 
 using namespace std;
 
-int main(){
+void sort(vector<int> &inputs){
+    
+}
 
+int main(){
+    int val;
+    vector<int> input;
+
+    cout << "Enter values seperated by spaces, then type a non-numeric value to enter them: ";
+    while(input.size() < 2){
+        while(cin >> val){
+            if(cin.good())input.push_back(val);
+        }
+        if(cin.fail()){
+            cin.clear();
+            cout << "\nList created.";
+            cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            cin.clear();
+            break;
+        }
+    }
+
+    cout << "\nYour list before the bubble sort: ";
+    for(int i = 0; i < input.size(); i++){
+        cout << ((i==input.size()-1)?to_string(input.at(i)):to_string(input.at(i))+", ");
+    }
+    cout << "\n\nSorting...";
+    sort(input);
+    cout << "\n\nYour list after the bubble sort: ";
+    for(int i = 0; i < input.size(); i++){
+        cout << ((i==input.size()-1)?to_string(input.at(i)):to_string(input.at(i))+", ");
+    }
     return 0;
 }
