69439b7cf93101ea8d5bf2980e892f11007a3910
qoride <qoride@gmail.com>
2024-10-13 02:54:25 +0000

    fully finished, added break points and iteration printing

--- main.cpp
+++ main.cpp
@@ -3,13 +3,25 @@
 #include <vector>
 
 using namespace std;
+int steps = 0;
 
 void sort(vector<int> &inputs){
+    bool sorted = false;
+    int swaps;
     for (int x = 0; x < inputs.size()-1; x++){
+        swaps = 0;
         for (int y = 0; y < inputs.size()-x-1; y++){
+            steps++;
             if(inputs.at(y) > inputs.at(y+1)){
+                steps++;
+                swaps++;
                 swap(inputs.at(y),inputs.at(y+1));
             }
+        }
+        if(swaps == 0)return;
+        cout << "\n\nYour list after " << x+1 << " iteration(s): ";
+        for(int i = 0; i < inputs.size(); i++){
+            cout << ((i==inputs.size()-1)?to_string(inputs.at(i)):to_string(inputs.at(i))+", ");
         }
     }
 }
@@ -36,8 +48,11 @@ int main(){
     for(int i = 0; i < input.size(); i++){
         cout << ((i==input.size()-1)?to_string(input.at(i)):to_string(input.at(i))+", ");
     }
+
     cout << "\n\nSorting...";
     sort(input);
+    cout << "\nFinished sorting in " << steps << " steps.";
+
     cout << "\n\nYour list after the bubble sort: ";
     for(int i = 0; i < input.size(); i++){
         cout << ((i==input.size()-1)?to_string(input.at(i)):to_string(input.at(i))+", ");
